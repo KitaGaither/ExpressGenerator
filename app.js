c75c29ae246d641ca73b28bba38013af065ffcbd
Lakita Gaither <lakitagaither@yahoo.com>
2021-09-06 23:36:25 -0500

    Express Session Part 2

--- app.js
+++ app.js
@@ -91,6 +91,27 @@ app.use('/promotions', promotionsRouter);
 app.use('/partners', partnerRouter);
 
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth(req, res, next) {
+    console.log(req.session);
+
+    if (!req.session.user) {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+    } else {
+        if (req.session.user === 'authenticated') {
+            return next();
+        } else {
+            const err = new Error('You are not authenticated!');
+            err.status = 401;
+            return next(err);
+        }
+    }
+}
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
