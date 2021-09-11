3f25aba3d7175cfe5be8a45de20ce6e57a9f539f
Lakita Gaither <lakitagaither@yahoo.com>
2021-09-11 10:02:40 -0500

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -1,20 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// const userSchema = new Schema({
+//     username: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     },
+//     admin: {
+//         type: Boolean,
+//         default: false
+//     }
+// });
+
 const userSchema = new Schema({
-    username: {
+    firstname: {
         type: String,
-        required: true,
-        unique: true
+        default: ''
     },
-    password: {
+    lastname: {
         type: String,
-        required: true
+        default: ''
     },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
 
 module.exports = mongoose.model('User', userSchema);
